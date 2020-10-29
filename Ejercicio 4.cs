9622a574d440f29988530c85f0aedd367b538a3a
Pamela Villar <71789070+pvm02@users.noreply.github.com>
2020-10-29 01:59:56 -0400

    Add files via upload

---
+++ Ejercicio 4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            } while (x < 11);
+        }
+    }
+}
+   
+
