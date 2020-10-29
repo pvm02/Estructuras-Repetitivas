70ea45a4ffb3b252bab1c9086948aab2e2284776
Pamela Villar <71789070+pvm02@users.noreply.github.com>
2020-10-29 02:17:03 -0400

    Add files via upload

---
+++ Ejercicio 10.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int edad;
+                Console.WriteLine("Ingresa tu edad");
+                edad = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error de formato");
+            }
+
+            int fecha;
+            Console.WriteLine("Ingresa tu fecha de nacimiento");
+            fecha = int.Parse(Console.ReadLine());
+        }
+    }
+}
