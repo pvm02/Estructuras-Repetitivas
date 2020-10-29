cee2981d528d41e82854ae668e2145e08b33b3a0
Pamela Villar <71789070+pvm02@users.noreply.github.com>
2020-10-29 02:09:29 -0400

    Add files via upload

---
+++ Ejercicio 2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int numero, contador;
+			contador = 0;
+
+			Console.Write("Introduce un número entero positivo: ");
+			numero = Convert.ToInt32(Console.ReadLine());
+
+			while (numero > 0)
+			{
+				numero = numero / 10;
+
+				contador = contador + 1;
+			}
+
+			Console.WriteLine("El número de digitos es {0}.", contador);
+		}
+	}
+}
+
